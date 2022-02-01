af086c86c715caf6eb2e208e0ea4d4a16392c57b
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-01 14:52:09 +0530

    Refactor PreviewHeader to be a functional component

--- src/components/Preview/PreviewHeader.js
+++ src/components/Preview/PreviewHeader.js
@@ -1,17 +1,11 @@
 import React from "react";
 
-class PreviewHeader extends React.Component {
-
-  render() {
-    const { firstName, lastName, title } = this.props.state.formValues;
-
-    return(
-      <div id="PreviewHeader">
-        <h1 id="UserName">{firstName} {lastName}</h1>
-        <p id="UserTitle" className="Subsection">{title}</p>
-      </div>
-    )
-  }
+export default function PreviewHeader({ state }) {
+  const { firstName, lastName, title } = state.formValues;
+  return (
+    <div id="PreviewHeader">
+      <h1 id="UserName">{firstName} {lastName}</h1>
+      <p id="UserTitle" className="Subsection">{title}</p>
+    </div>
+  )
 }
-
-export default PreviewHeader;
