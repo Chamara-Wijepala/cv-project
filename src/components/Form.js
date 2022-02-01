9ed2c59fcaa21ae4ee001f34875c94771defaa50
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-01 14:36:16 +0530

    Refactor Form component to be a functional component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,20 +3,13 @@ import GeneralInformation from "./Form/GeneralInformation";
 import Education from "./Form/Education";
 import PracticalExperience from "./Form/PracticalExperience";
 
-class Form extends React.Component {
-  
-  render() {
-    const { handleChange, handleSubmit } = this.props;
-
-    return(
-      <form id="Form" className="FlexItem" onSubmit={handleSubmit}>
-        <GeneralInformation handleChange={handleChange}></GeneralInformation>
-        <Education handleChange={handleChange}></Education>
-        <PracticalExperience handleChange={handleChange}></PracticalExperience>
-        <button type="submit" id="SubmitButton">Submit</button>
-      </form>
-    )
-  }
+export default function Form({ handleChange, handleSubmit }) {
+  return (
+    <form id="Form" className="FlexItem" onSubmit={handleSubmit}>
+      <GeneralInformation handleChange={handleChange}/>
+      <Education handleChange={handleChange}/>
+      <PracticalExperience handleChange={handleChange}/>
+      <button type="submit" id="SubmitButton">Submit</button>
+    </form>
+  )
 }
-
-export default Form;
