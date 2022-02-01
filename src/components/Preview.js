cf702cb9ddf0b7776cc1942faf9d161f2fe9ad28
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-01 19:04:00 +0530

    Refactor Preview and PreviewHeader

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,13 +2,13 @@ import React from "react";
 import PreviewHeader from "./Preview/PreviewHeader";
 import PreviewMain from "./Preview/PreviewMain";
 
-export default function Preview(props) {
-  const { state } = props;
+export default function Preview({ props }) {
+  const { formValues } = props;
 
   return(
     <div id="Preview" className="FlexItem">
-      <PreviewHeader state={state}/>
-      <PreviewMain state={state}/>
+      <PreviewHeader props={formValues}/>
+      <PreviewMain props={formValues}/>
     </div>
   )
 }
--- src/components/Preview/PreviewHeader.js
+++ src/components/Preview/PreviewHeader.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-export default function PreviewHeader({ state }) {
-  const { firstName, lastName, title } = state.formValues;
+export default function PreviewHeader({ props }) {
+  const { firstName, lastName, title } =  {...props };
+
   return (
     <div id="PreviewHeader">
       <h1 id="UserName">{firstName} {lastName}</h1>
